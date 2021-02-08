c90edb0cd53a24731e43876dd7a2994618aa0735
张浩锐 <Zhr199707>
2021-02-08 15:03:39 +0800

    日志实体类

---
+++ zhang.business/src/main/java/com/zhang/business/service/LogService.java
@@ -0,0 +1,13 @@
+package com.zhang.business.service;
+
+/**
+ *  日志服务
+ *  @Author： 张浩锐
+ */
+public interface LogService {
+
+
+    void saveLog(TStagingCommonLog tStagingCommonLog);
+
+
+}
--- zhang.dao/src/main/java/com/zhang/DaoStartApplication.java
+++ zhang.dao/src/main/java/com/zhang/DaoStartApplication.java
@@ -3,7 +3,7 @@ package com.zhang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.zhang.mapper" })
+@SpringBootApplication(scanBasePackages = {"com.zhang.dao.mapper"})
 public class DaoStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(DaoStartApplication.class, args);
